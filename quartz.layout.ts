55990201d6148378e4d407bcaeaaeb3993d783e5
Benjamin Meixner <46966993+maixnor@users.noreply.github.com>
2024-01-29 15:05:43 +0100

    no explorer and graph view

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -26,10 +26,8 @@ export const defaultContentPageLayout: PageLayout = {
     Component.MobileOnly(Component.Spacer()),
     Component.Search(),
     Component.Darkmode(),
-    Component.DesktopOnly(Component.Explorer()),
   ],
   right: [
-    Component.Graph(),
     Component.DesktopOnly(Component.TableOfContents()),
     Component.Backlinks(),
   ],
